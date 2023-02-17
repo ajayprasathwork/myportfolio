870b0d80deba1058cda6213e10fcc88eec5067c5
ajay <122023127+ajayprasathwork@users.noreply.github.com>
2023-02-17 16:03:38 +0530

    history card

--- src/page/fees.jsx
+++ src/page/fees.jsx
@@ -1,100 +1,114 @@
 import React from 'react';
 import './style.css'
-import { Space, Table, Tag,List} from 'antd';
+import { Button, Table, Alert, List, Checkbox, Col, Row } from 'antd';
 const Fees = () => {
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-          render: (text) => <a>{text}</a>,
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-        {
-          title: 'Tags',
-          key: 'tags',
-          dataIndex: 'tags',
-          render: (_, { tags }) => (
-            <>
-              {tags.map((tag) => {
-                let color = tag.length > 5 ? 'geekblue' : 'green';
-                if (tag === 'loser') {
-                  color = 'volcano';
-                }
-                return (
-                  <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                  </Tag>
-                );
-              })}
-            </>
-          ),
-        },
-        {
-          title: 'Action',
-          key: 'action',
-          render: (_, record) => (
-            <Space size="middle">
-              <a>Invite {record.name}</a>
-              <a>Delete</a>
-            </Space>
-          ),
-        },
-      ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          age: 32,
-          address: 'New York No. 1 Lake Park',
-          tags: ['nice', 'developer'],
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          age: 42,
-          address: 'London No. 1 Lake Park',
-          tags: ['loser'],
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          age: 32,
-          address: 'Sydney No. 1 Lake Park',
-          tags: ['cool', 'teacher'],
-        },
-      ];
-      const history = [
-        'Racing car sprays burning fuel into crowd.',
-        'Japanese princess to wed commoner.',
-        'Australian walks 100km after outback crash.',
-        'Man charged over missing wedding girl.',
-        'Los Angeles battles huge wildfires.',
-      ];
+  const onChange = (checkedValues) => {
+    console.log('checked = ', checkedValues);
+  };
+  const columns = [
+    {
+      title: 'Particulars',
+      dataIndex: 'Particulars',
+      key: 'Particulars',
+      render: (text) => <a>{text}</a>,
+      width: '90%',
+
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'Amount',
+      key: 'Amount',
+    },
+
+
+  ];
+  const data = [
+    {
+      key: '1',
+      Particulars: 'Special Fee 2023',
+      Amount: 6000,
+
+    },
+    {
+      key: '1',
+      Particulars: 'Fine',
+      Amount: 600,
+
+    },
+
+
+  ];
+  const history = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
+
   return (
     <div className='page'>
-        <div className='main-box'>
-          <h3 className='page-titel'>Fees</h3>
-        <Table columns={columns} dataSource={data} pagination={false}  title={() => 'Header'}
-              footer={() => <div className='table-fooder'></div>} />
+      <div className='main-box'>
+        <h3 className='page-titel'>Fees</h3>
+        <div className='stu-details'>
+          <p className='stu-name'>Student:<span>Ajay Prasath</span></p>
+          <p className='stu-name'>Class:<span>Grade-01-A 2023-24</span></p>
+          <p className='stu-name'>Admission No:<span>S1</span></p>
+
         </div>
-        <div className='side-box'>
+        <Table columns={columns} dataSource={data} pagination={false} title={() => <div className='table-header'>
+          <p className='tb-titel'>Spacial Fee</p>
+          <Alert className='due-date' message="DueDate: 01-04-2023" type="warning" showIcon />
+
+        </div>}
+          footer={() => <div className='table-fooder'>
+            <div className='table-header'>
+              <p className='tb-titel'>Total Amount To Pay</p>
+              <p className='total'>6600</p>
+            </div>
+            <div className='table-header pay-continer'>
+              <Checkbox.Group
+                style={{
+                  width: '100%',
+                }}
+                onChange={onChange}
+              >
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+                    <Checkbox value="A">A</Checkbox>
+
+              </Checkbox.Group>             
+              <Button type="primary">Pay Now</Button>
+            </div>
+          </div>} />
+
+      </div>
+      <div className='side-box'>
         <List
-        className='paymenthistory'
-      size="small"
-      header={<div>history</div>}
-      dataSource={history}
-      renderItem={(item) => <List.Item>{item}</List.Item>}
-    />        </div>
+          className='paymenthistory'
+          size="small"
+          header={<div className='hi-list-header'><p className='titel'>History</p><p className='viewall'>View all</p></div>}
+          dataSource={history}
+          renderItem={(item) => (
+            <List.Item key={item}>
+              <List.Item.Meta
+                title={<a href="https://ant.design">{item}</a>}
+                description={"pone"}
+
+              />
+              <div>
+                <Alert className='due-date' message="Padid" type="success" showIcon />
+
+              </div>
+
+            </List.Item>
+          )}
+        />
+      </div>
 
     </div>
   );
